4cbda8712c24f9b9a898503c8f682e4203785d15
xibo <1342092576@qq.com>
2019-08-23 19:47:50 +0800

    player

--- music_website/apps/repo/urls.py
+++ music_website/apps/repo/urls.py
@@ -5,5 +5,6 @@ from . import views
 urlpatterns = [
     url('^$',views.Index.as_view(),name='index'),
     url('^allsinger/$', views.AllSinger.as_view(), name='allsinger'),
-    url('^test/$',views.Test.as_view(),name='test'),
+    url('^player/$',views.Player.as_view(),name='player'),
+    url('^playerlist/$',views.Playerlist.as_view(),name='playerlist'),
 ]
--- music_website/apps/repo/views.py
+++ music_website/apps/repo/views.py
@@ -3,6 +3,8 @@ from django.views.generic import View
 from django.contrib.auth.mixins import LoginRequiredMixin
 from.models import Singer,MusicInfo
 from django.core.paginator import Paginator
+from django.http import JsonResponse
+from django.forms.models import model_to_dict
 
 # Create your views here.
 class Index(LoginRequiredMixin,View):
@@ -32,8 +34,32 @@ class AllSinger(LoginRequiredMixin,View):
         }
         return render(request,'allsinger.html',kwgs)
 
-from .forms import CommentsForm
-class Test(View):
-    def get(self,request):
-        form = CommentsForm()
-        return render(request,'test.html',{'form':form})
+class Player(View):
+    def post(self,request):
+        try:
+            id = request.POST.get('id')
+            music = MusicInfo.objects.get(id=id)
+            kwgs = {
+                'music': music
+            }
+        except Exception as e:
+            kwgs = {
+                'music':False
+            }
+        return render(request,'player.html',kwgs)
+
+class Playerlist(View):
+    def post(self,request):
+        try:
+            mid = request.POST.get('mid')
+            music = MusicInfo.objects.filter(singer_id=mid)
+            musicf = music[0]
+            kwgs = {
+                'music':music,
+                'musicf':musicf
+            }
+        except Exception as e:
+            kwgs = {
+                'code':400
+            }
+        return render(request,'player1.html',kwgs)
